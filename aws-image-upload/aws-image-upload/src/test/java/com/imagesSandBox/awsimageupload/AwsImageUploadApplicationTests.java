5d9187304a386c8339452ca1cabd7943b68a975f
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-11-06 19:04:27 -0400

    added the boiler plate code

---
+++ aws-image-upload/aws-image-upload/src/test/java/com/imagesSandBox/awsimageupload/AwsImageUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imagesSandBox.awsimageupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsImageUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
