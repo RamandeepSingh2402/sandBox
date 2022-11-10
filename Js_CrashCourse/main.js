961499ab35fb73bf956fc5e2c44967248b8f3f7f
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-11-10 19:50:45 -0400

    basics of assigning variables

--- Js_CrashCourse/main.js
+++ Js_CrashCourse/main.js
@@ -1,3 +1,24 @@
-console.log("Hello World!");
-console.error("This is an error");
-console.warn('This is a warning');
+// var, let and const
+// var has been used in the JS since beginning and is globally scoped
+// we dont use var anymore
+
+// let and const have block-level scope and both were added with the addition of ES-6 or ES-2015
+
+// What is the difference between let and const? :
+// Using let we can reassign the values
+
+
+
+/*
+    The following code block would give an error in the console, since age is mentioned as const
+    const age = 30;
+    age = 31;
+    console.log(age);
+*/
+
+/*
+    The following code block would display age as 31 in the console.
+    let age = 30;
+    age = 31;
+    console.log(age);
+*/
