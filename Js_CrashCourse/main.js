58d8d553a99d1313ff55dedcddeac5adefcc926c
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-11-24 18:19:02 -0400

    for of loop

--- Js_CrashCourse/main.js
+++ Js_CrashCourse/main.js
@@ -1,37 +1,3 @@
-// We'll be learning about object literals
-// Object literals are basically key-value pairs    
-
-const person = {
-    firstName: 'John',
-    lastName: 'Doe',
-    age: 30,
-    hobbies: ['music', 'movies', 'sports'],
-    address : {
-        street: '50 Main St',
-        city: 'Boston',
-        state: 'MA'
-    }
-}
-
-// accessing properties of objects
-console.log(person.firstName, person.lastName);
-
-// accessing properties of properties
-console.log(person.hobbies[1]);
-console.log(person.address.city);
-
-// using destructuring
-// pulling things out, meaning that we're pulling properties out of person
-const { firstName, lastName, address: {city} } = person;
-console.log(firstName, lastName, city);
-
-// we can add the properties to the object
-// On printing the object, the print will be done on the basis og  alphabetical order of properties
-person.email = 'john@gmail.com';
-console.log(person);
-
-
-// ARRAYS OF OBJECTS:
 const todos = [
     {
         id: 1,
@@ -50,25 +16,11 @@ const todos = [
     }
 ];
 
-console.log(todos);
-
-// printing from the array of the todos
-console.log(todos[2].text);
-
-
-// JSON
-const todoJSON = JSON.stringify(todos);
-console.log(todoJSON);
-
-// LOOPS IN JavaScript
-// for loop
-for (let i = 0; i <= 10; i++) {
-    console.log(`For Loop Number: ${i}`);
+for( let i = 0; i< todos.length; i++ ){
+    console.log(todos[i].text);
 }
 
-// while loop
-let i = 0;
-while (i < 10) {
-    console.log(`While Loop Number: ${i}`);
-    i++;
+// FOR-OF LOOP
+for(let todo of todos) {
+    console.log(todo.id);
 }
