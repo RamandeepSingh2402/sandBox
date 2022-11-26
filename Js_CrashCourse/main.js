1e25d36023abbc4b1460906d10eec77358c6b20c
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-11-26 12:37:28 -0400

    learnt about the ternary operator and the switches

--- Js_CrashCourse/main.js
+++ Js_CrashCourse/main.js
@@ -1,26 +1,25 @@
-// Conditionals
-const x = 4;
-const y = 11;
-
-/*
-    using '==' compares only the values, not the dataTypes
-    '===' compare the value on the basis of dataTypes as well
+// The ternary operator
+/* 
+    The TERNARY operator:
+    A shorthand if statement used alot to assign variables based on the condition
+    The first part is the condition and afte that is the question mark followed by teh color
+    and then is ':' which signifies else and then is the second condition.
 */
-if(x == 10) {
-    console.log(`The value of x without comparing thedata type is: ${x}`);
-}
-if(x === 10) {
-    console.log(`The value of x based on the data type as well is: ${x}`)
-}
-if(x > 5 && y > 11) {
-    console.log(`The value of x is more than 5 and the value of y is more than 10`);
-}
-if( x > 5 || y > 10) {
-    console.log('x is more than 5 or y is more than 10')
-}
-else if(x > 10) {
-    console.log(`The value of x is greater than 10`);
-}
-else {
-    console.log('The value of x is less than 10 and is ' + x + ' which is less than 10');
+const x = 9;
+
+const color = x > 10 ? 'Red' : 'Black'; 
+
+console.log(color);
+
+// SWITCHES
+switch(color) {
+    case 'red':
+        console.log(`The color is ${color}`);
+        break;
+    case 'blue':
+        console.log(`The color is ${color}`);
+        break;
+    default:
+        console.log(`The color is neither red nor black.`);
+        break;
 }
