d31223cf656043bc6f2092c36108e3cb2fbebc0a
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-11-26 12:53:24 -0400

    learnt more about the functions

--- Js_CrashCourse/main.js
+++ Js_CrashCourse/main.js
@@ -8,9 +8,16 @@
     would use the default value, but on the other hand, if a default value is provided,
     then whenever the function is called without any argument, then the function would be using it's default value.
 */
-function addNums(num1 = 1, num2 = 1) {
-     return (num1 + num2);
-}
+const addNums = num1 => num1 + 5;
 
-console.log(addNums(5,4));
-console.log(addNums());
+console.log(addNums(5));
+console.log(addNums(1));
+
+// todos.forEach((todo) => console.log(todo));
+
+const addingNum = (num1, num2) => {
+    console.log(`The sum of the nums is ${num1 + num2}`)
+}
+addingNum(2,3);
+
+// lexical "this" keyword
