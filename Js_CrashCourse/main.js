1efd53e51c3aa1a2e1bbc5ff088e205afbf142b6
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-11-26 12:45:02 -0400

    used the functions

--- Js_CrashCourse/main.js
+++ Js_CrashCourse/main.js
@@ -1,25 +1,16 @@
-// The ternary operator
+// FUNCTIONS
+
+// NaN means not a number
+
 /* 
-    The TERNARY operator:
-    A shorthand if statement used alot to assign variables based on the condition
-    The first part is the condition and afte that is the question mark followed by teh color
-    and then is ':' which signifies else and then is the second condition.
+    For the function below, we've used the default value for the nums
+    which means that if no argument is provided for the function, then the function 
+    would use the default value, but on the other hand, if a default value is provided,
+    then whenever the function is called without any argument, then the function would be using it's default value.
 */
-const x = 9;
-
-const color = x > 10 ? 'Red' : 'Black'; 
-
-console.log(color);
-
-// SWITCHES
-switch(color) {
-    case 'red':
-        console.log(`The color is ${color}`);
-        break;
-    case 'blue':
-        console.log(`The color is ${color}`);
-        break;
-    default:
-        console.log(`The color is neither red nor black.`);
-        break;
+function addNums(num1 = 1, num2 = 1) {
+     return (num1 + num2);
 }
+
+console.log(addNums(5,4));
+console.log(addNums());
