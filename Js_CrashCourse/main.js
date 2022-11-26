ac9448e0cd76e1303fbeacb0542c46f2ee1de90e
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-11-25 21:01:49 -0400

    practised the conditionals again

--- Js_CrashCourse/main.js
+++ Js_CrashCourse/main.js
@@ -1,56 +1,26 @@
-const todos = [
-    {
-        id: 1,
-        text: 'Take out trash',
-        isComplete: true
-    },
-    {
-        id: 2,
-        text: 'Meeting with boss',
-        isComplete: true
-    },
-    {
-        id: 3,
-        text: 'Dentist appointment',
-        isComplete: false
-    }
-];
-
-// high order array methods
-// forEach (loops through the arrays)
-// map (allows us to create a new array from an array)
-// filter (allows us to create an array based on a condition)
+// Conditionals
+const x = 4;
+const y = 11;
 
 /*
-    These high order methods take in parameter of function and the call-back
-    function takes in a parameter which is a variable  that we want to use 
-    as each item (for this each todo )
+    using '==' compares only the values, not the dataTypes
+    '===' compare the value on the basis of dataTypes as well
 */
-
-
-// forEach 
-todos.forEach(function(todo) {
-    console.log(todo.text);
-});
-
-// map
-// with map it actually returns an array
-const todoText = todos.map(function(mapTodo) {
-    return mapTodo.text;
-});
-console.log(`The text for todos using map is: ${todoText}`);
-
-//filter
-const completedTodos = todos.filter(function(filterCompleted) {
-    return filterCompleted.isComplete === true;
-}).map(function(filterCompleted){
-    return filterCompleted.text
-});
-console.log(completedTodos);
-/*
-
-    One thing to note here is that simple console.log(completedTodos) gives us 
-    the whole array which can be inspected, but on the other hand, 
-    completedTodos with the text give us objects which can't be inspected.
-*/
-console.log(`The text of the filtered and completed todos is: ${completedTodos}`);
+if(x == 10) {
+    console.log(`The value of x without comparing thedata type is: ${x}`);
+}
+if(x === 10) {
+    console.log(`The value of x based on the data type as well is: ${x}`)
+}
+if(x > 5 && y > 11) {
+    console.log(`The value of x is more than 5 and the value of y is more than 10`);
+}
+if( x > 5 || y > 10) {
+    console.log('x is more than 5 or y is more than 10')
+}
+else if(x > 10) {
+    console.log(`The value of x is greater than 10`);
+}
+else {
+    console.log('The value of x is less than 10 and is ' + x + ' which is less than 10');
+}
