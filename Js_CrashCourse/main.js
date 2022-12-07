a0a7fa3eddc5031fa8d455e25dbb41fb83e485c4
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-12-06 21:34:16 -0400

    commiting the revision of the eventListeneres

--- Js_CrashCourse/main.js
+++ Js_CrashCourse/main.js
@@ -1,20 +1,25 @@
-const ul = document.querySelector('.items');
-
-// changing the content of the first li
-ul.firstElementChild.textContent = 'Hello from the changed content of the first li';
-
-// using innerText of the returned node list
-ul.children[1].innerText = 'Brad';
-
-// changing the content of the last item
-ul.lastElementChild.innerHTML  = '<h1>Hello from h1</h1>';
-
-// removing the last item
-ul.lastElementChild.remove();
-
-// removes the whole ul
-ul.remove();
-
-
+// using eventListeners
 const btn = document.querySelector('.btn');
-btn.style.background = 'red';
+
+/**
+ * There are multiple types of events which we can have as event listeners
+ * some of them are click, mouseover(works for hovering the mouse over), mouseout (when we exit the element, thats when the event is fired)
+ * there are more events like input events as well
+ */
+btn.addEventListener('click', (event) => {
+    event.preventDefault();
+    document.querySelector('#my-form').style.background = '#ccc';
+    // to add a class, we do classList.add() and to remove we do classList.remove()
+
+    // for this, it changes the form background to black and the text color to white
+    document.querySelector('body').classList.add('bg-dark');
+
+    document.querySelector('.items').lastElementChild.innerHTML = '<h1>Hello</h1>';
+
+    // btn.style.background = 'red';
+    // console.log(`The button was clicked`);
+    // console.log(`The event object looks like this:`);
+    
+    // event.target returns the target on which the eventListener is placed
+    // console.log(event.target.className);
+} );
