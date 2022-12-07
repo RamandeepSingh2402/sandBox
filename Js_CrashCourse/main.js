2599e7ca12b124e091ba710fc8a1eaf36ebbe019
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-12-06 21:52:18 -0400

    commiting the changes with eventListeners and setTimeOuts

--- Js_CrashCourse/main.js
+++ Js_CrashCourse/main.js
@@ -1,25 +1,30 @@
-// using eventListeners
-const btn = document.querySelector('.btn');
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
 
-/**
- * There are multiple types of events which we can have as event listeners
- * some of them are click, mouseover(works for hovering the mouse over), mouseout (when we exit the element, thats when the event is fired)
- * there are more events like input events as well
- */
-btn.addEventListener('click', (event) => {
-    event.preventDefault();
-    document.querySelector('#my-form').style.background = '#ccc';
-    // to add a class, we do classList.add() and to remove we do classList.remove()
+myForm.addEventListener('submit', onSubmit);
 
-    // for this, it changes the form background to black and the text color to white
-    document.querySelector('body').classList.add('bg-dark');
+function onSubmit(e) {
+    e.preventDefault();
+    console.log(nameInput.value);
+    if(nameInput.value === "" || emailInput.value === "") {
+        msg.classList.add('error');
+        msg.innerHTML ='Please enter all fields.';
 
-    document.querySelector('.items').lastElementChild.innerHTML = '<h1>Hello</h1>';
+        // using the setTimeout() function which removes the msg after 3000ms or 3 seconds
+        setTimeout(() => msg.remove(), 3000);
 
-    // btn.style.background = 'red';
-    // console.log(`The button was clicked`);
-    // console.log(`The event object looks like this:`);
-    
-    // event.target returns the target on which the eventListener is placed
-    // console.log(event.target.className);
-} );
+    }
+    else {
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+
+        userList.appendChild(li);
+
+        // clear the fields
+        nameInput.value = '';
+        emailInput.value = '';
+    }
+}
